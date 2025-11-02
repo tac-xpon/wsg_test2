b1fd53ef085dde9c0c8c5806056433cd82cb73a5
Tsujimura Takuya <xpon093018@tochigiweb.jp>
2025-11-02 10:43:57 +0900

    InputRoleの初期化部分をブラッシュアップ 画面上のPlaySpeed変更UI(-+ボタン)の位置変更 画面上にSoundNo変更UI(-+ボタン)追加 画面上にVolume変更UI(-+ボタン)追加 追加-+ボタンの判別のためにBGパレット追加 マウスUI操作とキー操作による動作を統合

--- src/main.rs
+++ src/main.rs
@@ -58,61 +58,140 @@ fn main() {
     );
 
     let mut keyboard_map = InputRoleMap::<Key>::new();
-    {
-        let set_list = [
-            (Key::Space, InputRole::Start),
-            (Key::D4,    InputRole::Pause),
-            (Key::Return,InputRole::Ok),
-            (Key::X,     InputRole::Cancel),
-            (Key::W,     InputRole::Up),
-            (Key::D,     InputRole::Right),
-            (Key::S,     InputRole::Down),
-            (Key::A,     InputRole::Left),
-            (Key::Up,    InputRole::Up),
-            (Key::Right, InputRole::Right),
-            (Key::Down,  InputRole::Down),
-            (Key::Left,  InputRole::Left),
-        ];
-        keyboard_map.assign(&set_list);
-    }
+    keyboard_map.assign(&[
+        (Key::Space, InputRole::Start),
+        (Key::D4, InputRole::Pause),
+        (Key::Return, InputRole::Ok),
+        (Key::X, InputRole::Cancel),
+        (Key::W, InputRole::Up),
+        (Key::D, InputRole::Right),
+        (Key::S, InputRole::Down),
+        (Key::A, InputRole::Left),
+        (Key::Up, InputRole::Up),
+        (Key::Right, InputRole::Right),
+        (Key::Down, InputRole::Down),
+        (Key::Left, InputRole::Left),
+    ]);
     let mut button_map = InputRoleMap::<ControllerButton>::new();
-    {
-        let set_list = [
-            (ControllerButton { id: 0, button: 0 }, InputRole::Start),
-            (ControllerButton { id: 0, button: 1 }, InputRole::Pause),
-            (ControllerButton { id: 0, button: 2 }, InputRole::Ok),
-            (ControllerButton { id: 0, button: 3 }, InputRole::Cancel),
-            (ControllerButton { id: 0, button: 3 }, InputRole::Cancel),
-        ];
-        button_map.assign(&set_list);
-    }
+    button_map.assign(&[
+        (ControllerButton { id: 0, button: 0 }, InputRole::Start),
+        (ControllerButton { id: 0, button: 1 }, InputRole::Pause),
+        (ControllerButton { id: 0, button: 2 }, InputRole::Ok),
+        (ControllerButton { id: 0, button: 3 }, InputRole::Cancel),
+        (ControllerButton { id: 0, button: 3 }, InputRole::Cancel),
+    ]);
     let mut hat_map = InputRoleMap::<ControllerHat>::new();
-    {
-        let set_list = [
-            (ControllerHat { id: 0, which: 0, state: HatState::Centered  }, InputRole::None),
-            (ControllerHat { id: 0, which: 0, state: HatState::Up        }, InputRole::Up),
-            (ControllerHat { id: 0, which: 0, state: HatState::Down      }, InputRole::Down),
-            (ControllerHat { id: 0, which: 0, state: HatState::Right     }, InputRole::Right),
-            (ControllerHat { id: 0, which: 0, state: HatState::Left      }, InputRole::Left),
-            (ControllerHat { id: 0, which: 0, state: HatState::RightUp   }, InputRole::Right),
-            (ControllerHat { id: 0, which: 0, state: HatState::RightUp   }, InputRole::Up),
-            (ControllerHat { id: 0, which: 0, state: HatState::RightDown }, InputRole::Right),
-            (ControllerHat { id: 0, which: 0, state: HatState::RightDown }, InputRole::Down),
-            (ControllerHat { id: 0, which: 0, state: HatState::LeftUp    }, InputRole::Left),
-            (ControllerHat { id: 0, which: 0, state: HatState::LeftUp    }, InputRole::Up),
-            (ControllerHat { id: 0, which: 0, state: HatState::LeftDown  }, InputRole::Left),
-            (ControllerHat { id: 0, which: 0, state: HatState::LeftDown  }, InputRole::Down),
-        ];
-        hat_map.assign(&set_list);
-    }
+    hat_map.assign(&[
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::Centered,
+            },
+            InputRole::None,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::Up,
+            },
+            InputRole::Up,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::Down,
+            },
+            InputRole::Down,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::Right,
+            },
+            InputRole::Right,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::Left,
+            },
+            InputRole::Left,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::RightUp,
+            },
+            InputRole::Right,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::RightUp,
+            },
+            InputRole::Up,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::RightDown,
+            },
+            InputRole::Right,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::RightDown,
+            },
+            InputRole::Down,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::LeftUp,
+            },
+            InputRole::Left,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::LeftUp,
+            },
+            InputRole::Up,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::LeftDown,
+            },
+            InputRole::Left,
+        ),
+        (
+            ControllerHat {
+                id: 0,
+                which: 0,
+                state: HatState::LeftDown,
+            },
+            InputRole::Down,
+        ),
+    ]);
     let mut mousebutton_map = InputRoleMap::<MouseButton>::new();
-    {
-        let set_list = [
-            (MouseButton::Left,  InputRole::LeftButton),
-            (MouseButton::Right, InputRole::RightButton),
-        ];
-        mousebutton_map.assign(&set_list);
-    }
+    mousebutton_map.assign(&[
+        (MouseButton::Left, InputRole::LeftButton),
+        (MouseButton::Right, InputRole::RightButton),
+    ]);
     let mut input_role_state = InputRoleState::default();
 
     let mut bg_texture_bank = BgTextureBank::new(
@@ -160,23 +239,33 @@ fn main() {
     {
         let s = "Test for WSG Play".to_string();
         let x = (VM_RECT_SIZE.0 - s.len() as i32) / 2;
-        bg.1.set_cur_pos(x, 0).put_string(&s, Some(&CharAttributes::new(3, BgSymmetry::Normal)));
-        bg.1.set_cur_pos(1, 4).put_achar(&AChar::new('C', 4, BgSymmetry::Normal));
-        bg.1.set_cur_pos(4, 4).put_achar(&AChar::new(0x80 as u32, 4, BgSymmetry::Normal));
+        bg.1.set_cur_pos(x, 0)
+            .put_string(&s, Some(&CharAttributes::new(3, BgSymmetry::Normal)));
+        bg.1.set_cur_pos(1, 4)
+            .put_achar(&AChar::new('C', 4, BgSymmetry::Normal));
+        bg.1.set_cur_pos(4, 4)
+            .put_achar(&AChar::new(0x80 as u32, 4, BgSymmetry::Normal));
         for i in 0..8 {
             let y = 6 + i as i32 * 2;
             let achar = AChar::new('0' as u32 + i, 4, BgSymmetry::Normal);
-            bg.1.set_cur_pos( 0, y).put_string("LCR", Some(&CharAttributes::new(5, BgSymmetry::Normal)));
-            bg.1.set_cur_pos( 4, y).put_achar(&achar).put_achar(&AChar::new(':', 1, BgSymmetry::Normal));
+            bg.1.set_cur_pos(0, y)
+                .put_string("LCR", Some(&CharAttributes::new(5, BgSymmetry::Normal)));
+            bg.1.set_cur_pos(4, y)
+                .put_achar(&achar)
+                .put_achar(&AChar::new(':', 1, BgSymmetry::Normal));
             bg.1.set_cur_pos(17, y).put_palette_n(3, 18).put_palette(2);
         }
         bg.1.set_cur_pos(8, 3).put_string(&"LastFrame", None);
         bg.1.set_achar_at(12, 4, &AChar::new(0x7fu32, 4, BgSymmetry::Normal));
         bg.1.set_cur_pos(18, 3).put_string(&"PlaySpeed", None);
-        bg.1.set_achar_at(20, 4, &AChar::new('-', 4, BgSymmetry::Normal));
-        bg.1.set_achar_at(25, 4, &AChar::new('+', 4, BgSymmetry::Normal));
+        bg.1.set_achar_at(25, 4, &AChar::new('-', 4, BgSymmetry::Normal));
+        bg.1.set_achar_at(26, 4, &AChar::new('+', 4, BgSymmetry::Normal));
         bg.1.set_cur_pos(30, 3).put_string(&"Sound:", None);
+        bg.1.set_achar_at(38, 3, &AChar::new('-', 6, BgSymmetry::Normal));
+        bg.1.set_achar_at(39, 3, &AChar::new('+', 6, BgSymmetry::Normal));
         bg.1.set_cur_pos(29, 4).put_string(&"Volume:", None);
+        bg.1.set_achar_at(38, 4, &AChar::new('-', 7, BgSymmetry::Normal));
+        bg.1.set_achar_at(39, 4, &AChar::new('+', 7, BgSymmetry::Normal));
     }
     spr.sp[0].code(0).palette(1).symmetry(SpSymmetry::Normal);
 
@@ -212,18 +301,39 @@ fn main() {
             buffer_remain += samples_per_frame_2ch;
         }
 
-        let (m_pos_spx, m_pos_spy) = (pointer_pos.0 as i32 / PIXEL_SCALE, pointer_pos.1 as i32 / PIXEL_SCALE);
-        let (m_pos_bgx, m_pos_bgy) = (m_pos_spx / PATTERN_SIZE as i32, m_pos_spy / PATTERN_SIZE as i32);
-        bg.0.set_cur_pos(0, 0).put_string(&format!("{:4}", t_count), Some(&CharAttributes::new(1, BgSymmetry::Normal)));
-        bg.0.set_cur_pos(0, 1).put_string(&format!("{:3} {:3}", m_pos_spx, m_pos_spy), Some(&CharAttributes::new(1, BgSymmetry::Normal)));
-        bg.0.set_cur_pos(0, 2).put_string(&format!("{:3} {:3}", m_pos_bgx, m_pos_bgy), Some(&CharAttributes::new(1, BgSymmetry::Normal)));
+        let (m_pos_spx, m_pos_spy) = (
+            pointer_pos.0 as i32 / PIXEL_SCALE,
+            pointer_pos.1 as i32 / PIXEL_SCALE,
+        );
+        let (m_pos_bgx, m_pos_bgy) = (
+            m_pos_spx / PATTERN_SIZE as i32,
+            m_pos_spy / PATTERN_SIZE as i32,
+        );
+        bg.0.set_cur_pos(0, 0).put_string(
+            &format!("{:4}", t_count),
+            Some(&CharAttributes::new(1, BgSymmetry::Normal)),
+        );
+        bg.0.set_cur_pos(0, 1).put_string(
+            &format!("{:3} {:3}", m_pos_spx, m_pos_spy),
+            Some(&CharAttributes::new(1, BgSymmetry::Normal)),
+        );
+        bg.0.set_cur_pos(0, 2).put_string(
+            &format!("{:3} {:3}", m_pos_bgx, m_pos_bgy),
+            Some(&CharAttributes::new(1, BgSymmetry::Normal)),
+        );
         spr.sp[0].pos(Pos::new(m_pos_spx, m_pos_spy)).visible(true);
 
-        let mut selected = None;
-        if input_role_state.get(InputRole::LeftButton).1 & 0b1111 == 0b0011 {
+        let mut selected:Option<usize> = None;
+        let mut speed_ctl:Option<Direction> = None;
+        let mut music_select_ctl:Option<Direction> = None;
+        let mut gain_ctl:Option<Direction> = None;
+        if input_role_state.get(InputRole::LeftButton).1 & 0b1111 == 0b0011
+            || input_role_state.get(InputRole::LeftButton).1 & 0xfff_ffff == 0xfff_ffff
+                && t_count % 8 == 0
+        {
             bg.1.set_cur_pos(m_pos_bgx, m_pos_bgy);
             let achar = bg.1.read_achar();
-            if achar.palette == 4 || achar.palette == 5 {
+            if (4..=7).contains(&achar.palette) {
                 if ('0' as u32..='7' as u32).contains(&achar.code) {
                     let ch = achar.code as usize - '0' as usize;
                     sound_generator.mute[ch] = !sound_generator.mute[ch];
@@ -240,13 +350,19 @@ fn main() {
                     }
                 }
                 if achar.code == '+' as u32 {
-                    if play_step > 1 {
-                        play_step -= 1;
+                    match achar.palette {
+                        4 => speed_ctl = Some(Direction::Up),
+                        6 => music_select_ctl = Some(Direction::Up),
+                        7 => gain_ctl = Some(Direction::Up),
+                        _ => ()
                     }
                 }
                 if achar.code == '-' as u32 {
-                    if play_step < 4 {
-                        play_step += 1;
+                    match achar.palette {
+                        4 => speed_ctl = Some(Direction::Down),
+                        6 => music_select_ctl = Some(Direction::Down),
+                        7 => gain_ctl = Some(Direction::Down),
+                        _ => ()
                     }
                 }
                 if achar.code == 0x7f {
@@ -272,22 +388,28 @@ fn main() {
         }
 
         if input_role_state.get(InputRole::Left).1 & 0b1111 == 0b0011
-        || input_role_state.get(InputRole::Left).1 & 0xfff_ffff == 0xfff_ffff && t_count % 4 == 0 {
-            music_select = if music_select == 0 { 31 } else { music_select -1 };
+            || input_role_state.get(InputRole::Left).1 & 0xfff_ffff == 0xfff_ffff
+                && t_count % 4 == 0
+        {
+            music_select_ctl = Some(Direction::Down);
         }
         if input_role_state.get(InputRole::Right).1 & 0b1111 == 0b0011
-        || input_role_state.get(InputRole::Right).1 & 0xfff_ffff == 0xfff_ffff && t_count % 4 == 0 {
-            music_select = if music_select == 31 { 0 } else { music_select + 1 };
+            || input_role_state.get(InputRole::Right).1 & 0xfff_ffff == 0xfff_ffff
+                && t_count % 4 == 0
+        {
+            music_select_ctl = Some(Direction::Up);
         }
-        if input_role_state.get(InputRole::Up).1 & 0b1111 == 0b0011 {
-            if master_gain < 7 {
-                master_gain += 1;
-            }
+        if input_role_state.get(InputRole::Up).1 & 0b1111 == 0b0011
+            || input_role_state.get(InputRole::Up).1 & 0xfff_ffff == 0xfff_ffff
+                && t_count % 8 == 0
+        {
+            gain_ctl = Some(Direction::Up);
         }
-        if input_role_state.get(InputRole::Down).1 & 0b1111 == 0b0011 {
-            if master_gain > 0 {
-                master_gain -= 1;
-            }
+        if input_role_state.get(InputRole::Down).1 & 0b1111 == 0b0011
+            || input_role_state.get(InputRole::Down).1 & 0xfff_ffff == 0xfff_ffff
+                && t_count % 8 == 0
+        {
+            gain_ctl = Some(Direction::Down);
         }
         if input_role_state.get(InputRole::Start).1 & 0b1111 == 0b0011 {
             if let Some(music_no) = music_playing {
@@ -307,6 +429,24 @@ fn main() {
             }
         }
 
+
+        play_step += match speed_ctl {
+            Some(Direction::Up) if play_step > 1 => -1,
+            Some(Direction::Down) if play_step < 4 => 1,
+            _ => 0,
+        };
+        music_select += match music_select_ctl {
+            Some(Direction::Up) => 1,
+            Some(Direction::Down) => 31,
+            _ => 0,
+        };
+        music_select %= 32;
+        master_gain += match gain_ctl {
+            Some(Direction::Up) if master_gain < 7 => 1,
+            Some(Direction::Down) if master_gain > 0 => -1,
+            _ => 0,
+        };
+
         if let Some(music_no) = selected {
             if music_no == 0x1f {
                 sound_manager.play_request[music_no] += 1;
@@ -318,7 +458,10 @@ fn main() {
             }
         }
         for music_no in 0..0x20 {
-            if bg.1.get_code_at(music_no as i32 + 4,22) == 'R' as u32 && !sound_manager.play_progress(music_no) && sound_manager.play_request[music_no] == 0 {
+            if bg.1.get_code_at(music_no as i32 + 4, 22) == 'R' as u32
+                && !sound_manager.play_progress(music_no)
+                && sound_manager.play_request[music_no] == 0
+            {
                 sound_manager.play_request[music_no] = -1;
             }
         }
@@ -331,40 +474,62 @@ fn main() {
         for ch in 0..NUM_OF_AUDIO_CHANNELS {
             let (w, f, g) = sound_data[ch];
             let freq = f as f64 / FREQ_ADJ_RATIO;
-            let gain = if sound_generator.mute[ch] || freq <= 30.0 { 0 } else { g as i32 };
+            let gain = if sound_generator.mute[ch] || freq <= 30.0 {
+                0
+            } else {
+                g as i32
+            };
             let y = (6 + ch * 2) as i32;
             bg.1.set_palette_n_at(0, y, 5, 3);
             bg.1.set_palette_at(1 + sound_generator.panpot[ch] as i32, y, 3);
             bg.1.set_palette_at(4, y, if sound_generator.mute[ch] { 5 } else { 4 });
-            bg.1.set_cur_pos( 6, y).put_string(&format!("{:7.2}Hz {:1} {:2} ", freq, w, gain), None);
-            bg.1.set_cur_pos(21, y).put_code_n(0x7f as u32, gain).put_code_n(' ', 15 - gain);
+            bg.1.set_cur_pos(6, y)
+                .put_string(&format!("{:7.2}Hz {:1} {:2} ", freq, w, gain), None);
+            bg.1.set_cur_pos(21, y)
+                .put_code_n(0x7f as u32, gain)
+                .put_code_n(' ', 15 - gain);
         }
         bg.1.set_cur_pos(4, 22);
         for music_no in 0..0x20 {
             let c = match sound_manager.play_request[music_no] {
                 0 => ' ',
-                n => if n > 0 { (n as u8 + '0' as u8) as char } else { 'R' }
+                n => {
+                    if n > 0 {
+                        (n as u8 + '0' as u8) as char
+                    } else {
+                        'R'
+                    }
+                }
             };
             bg.1.put_achar(&AChar::new(c, 1, BgSymmetry::Normal));
         }
         bg.1.set_cur_pos(4, 23);
         for music_no in 0..0x20 {
-            let c = if sound_manager.play_progress(music_no) { '|' } else { '_' };
+            let c = if sound_manager.play_progress(music_no) {
+                '|'
+            } else {
+                '_'
+            };
             bg.1.put_achar(&AChar::new(c, 4, BgSymmetry::Normal));
         }
         bg.1.set_code_n_at(4, 24, ' ', 0x20);
         bg.1.set_cur_pos(music_select as i32 + 4, 24);
         bg.1.put_achar(&AChar::new('^', 3, BgSymmetry::Normal));
-        #[cfg(feature="develop")]
+        #[cfg(feature = "develop")]
         {
             if input_role_state.get(InputRole::Pause).1 & 0b1111 == 0b0011 {
                 println!("{:?}", sound_manager.get_ch_registers());
                 println!("{:?}", sound_manager.play_request);
             }
         }
-        bg.1.set_cur_pos(24, 2).put_string(&format!("{:9} {:6}", audio_device.current(), audio_device.remain()), Some(&CharAttributes::new(2, BgSymmetry::Normal)));
-        bg.1.set_cur_pos(36, 3).put_string(&format!("{:02X}", music_select), None);
-        bg.1.set_cur_pos(37, 4).put_string(&format!("{:1}", master_gain), None);
+        bg.1.set_cur_pos(24, 2).put_string(
+            &format!("{:9} {:6}", audio_device.current(), audio_device.remain()),
+            Some(&CharAttributes::new(2, BgSymmetry::Normal)),
+        );
+        bg.1.set_cur_pos(36, 3)
+            .put_string(&format!("{:02X}", music_select), None);
+        bg.1.set_cur_pos(37, 4)
+            .put_string(&format!("{:1}", master_gain), None);
         let speed = match play_step {
             1 => 100,
             2 => 50,
@@ -372,7 +537,8 @@ fn main() {
             4 => 25,
             _ => 0,
         };
-        bg.1.set_cur_pos(21, 4).put_string(&format!("{:3}%", speed), None);
+        bg.1.set_cur_pos(21, 4)
+            .put_string(&format!("{:3}%", speed), None);
         let p = if suppress_last { 5 } else { 4 };
         bg.1.set_palette_at(12, 4, p);
 
